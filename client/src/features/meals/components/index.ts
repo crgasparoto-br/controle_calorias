b835f5dd0f44c0338bbee75d8978b9a329dfd523
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-05-21 15:23:30 -0300

    refactor: export meal mode guide

--- client/src/features/meals/components/index.ts
+++ client/src/features/meals/components/index.ts
@@ -1,4 +1,5 @@
 export { DayNavigator } from "./DayNavigator";
 export { MealItemEditor } from "./MealItemEditor";
+export { MealModeGuide } from "./MealModeGuide";
 export { RegisteredMealGroups } from "./RegisteredMealGroups";
 export { SummaryPill } from "./SummaryPill";
