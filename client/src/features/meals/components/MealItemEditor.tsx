47f4ceed8cefeb9d0664a25b4361ee2496c07dc3
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-06-06 18:42:22 -0300

    fix: use shared measurement unit alias

--- client/src/features/meals/components/MealItemEditor.tsx
+++ client/src/features/meals/components/MealItemEditor.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { trpc } from "@/lib/trpc";
-import { MEASUREMENT_UNIT_SUGGESTIONS, normalizeMeasurementUnit } from "@/shared/measurementUnits";
+import { MEASUREMENT_UNIT_SUGGESTIONS, normalizeMeasurementUnit } from "@shared/measurementUnits";
 import type { MealItemState } from "../types";
 
 type MealItemEditorProps = {
