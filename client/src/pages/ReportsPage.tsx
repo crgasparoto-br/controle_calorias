ea99c2fcb8566c90b64334c22e6c482936a8ea5d
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-06-23 16:44:12 -0300

    Fix React import in reports page

--- client/src/pages/ReportsPage.tsx
+++ client/src/pages/ReportsPage.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import DashboardLayout from "@/components/DashboardLayout";
 import ReportsGoalInsightsPanel from "@/components/ReportsGoalInsightsPanel";
 import ReportsExperience from "@/features/reports/ReportsExperience";
