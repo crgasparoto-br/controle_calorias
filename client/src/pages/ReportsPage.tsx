932d9fa0d2a9f73b3205ed6dce013b797243ac76
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-06-23 17:13:39 -0300

    Keep reports page as shared experience wrapper

--- client/src/pages/ReportsPage.tsx
+++ client/src/pages/ReportsPage.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import DashboardLayout from "@/components/DashboardLayout";
-import ReportsGoalInsightsPanel from "@/components/ReportsGoalInsightsPanel";
 import ReportsExperience from "@/features/reports/ReportsExperience";
 
 export default function ReportsPage() {
   return (
     <DashboardLayout>
       <ReportsExperience context="self" />
-      <ReportsGoalInsightsPanel />
     </DashboardLayout>
   );
 }
