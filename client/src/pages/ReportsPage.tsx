901bcae6c3161fbd53a5f46dc3adb7b73d66954c
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-06-23 16:20:59 -0300

    Keep reports goal insights on reports page

--- client/src/pages/ReportsPage.tsx
+++ client/src/pages/ReportsPage.tsx
@@ -1,10 +1,12 @@
 import DashboardLayout from "@/components/DashboardLayout";
+import ReportsGoalInsightsPanel from "@/components/ReportsGoalInsightsPanel";
 import ReportsExperience from "@/features/reports/ReportsExperience";
 
 export default function ReportsPage() {
   return (
     <DashboardLayout>
       <ReportsExperience context="self" />
+      <ReportsGoalInsightsPanel />
     </DashboardLayout>
   );
 }
