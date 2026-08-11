71e5af4a19d0107a9d353b1d7d3409bf872bdb28
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-08-11 19:34:04 -0300

    fix(ci): reuse billing database provider in Asaas store

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,19 @@
 import {
+  getDb as getDbImplementation,
   logInferenceEvent as logInferenceEventImplementation,
 } from "./dbImplementation";
 import { normalizeImageAnnotationInferenceEvent } from "./modules/whatsapp/imageAnnotationTelemetryContext";
+import { getConfiguredBillingDbProvider } from "./repositories/billingRepositorySupport";
 
 export * from "./dbImplementation";
+
+export async function getDb() {
+  const billingProvider = getConfiguredBillingDbProvider();
+  if (billingProvider && billingProvider !== getDb) {
+    return billingProvider();
+  }
+  return getDbImplementation();
+}
 
 export function logInferenceEvent(
   input: Parameters<typeof logInferenceEventImplementation>[0],
