0bb2fb30867cc2e25bb3af34dcb0ad630d1ad3d7
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-09-02 13:22:32 -0300

    test(login): provide React runtime for direct render

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { trpc } from "@/lib/trpc";
 import { Eye, EyeOff } from "lucide-react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link } from "wouter";
 import calorieControlIcon from "../../../imagens/premium_app_icon_for_a_smart_calorie_control_2.png";
 
